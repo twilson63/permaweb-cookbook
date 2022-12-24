fa92881fb40f57b80da15dde3de044610f86317c
Tom Wilson <tom@hyper.io>
2022-12-24 12:56:11 +0000

    chore(deploy): changed warp to use Arweave Gateway instead of sequencer, ArNS needs interactions to be readable on the gateway

--- docs/deploy.mjs
+++ docs/deploy.mjs
@@ -1,5 +1,5 @@
 import Bundlr from '@bundlr-network/client'
-import { WarpFactory } from 'warp-contracts'
+import { WarpFactory, defaultCacheOptions } from 'warp-contracts'
 import fs from 'fs'
 import Arweave from 'arweave'
 
@@ -8,7 +8,12 @@ const ANT = 'YcUbL7_j2DLxvMX0dqOu8N73mrcCRzovsE5Mw22uLmc'
 const arweave = Arweave.init({ host: 'arweave.net', port: 443, protocol: 'https' })
 const jwk = JSON.parse(fs.readFileSync('../wallet.json', 'utf-8'))
 const bundlr = new Bundlr.default('https://node2.bundlr.network', 'arweave', jwk)
-const warp = WarpFactory.forMainnet({ useArweaveGw: true, arweave })
+const warp = WarpFactory.custom(
+  arweave,
+  defaultCacheOptions,
+  'mainnet'
+).useArweaveGateway().build()
+
 const contract = warp.contract(ANT).connect(jwk)
 // upload folder
 const result = await bundlr.uploadFolder('./src/.vuepress/dist', {
