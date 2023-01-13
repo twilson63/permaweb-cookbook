1dde35e7f3b7512ed4b75e2905060f51226f60cd
Dan MacDonald <samuraidan@gmail.com>
2023-01-13 15:25:04 -0800

    fix: repair the container plugin integration (#101)

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,5 +1,6 @@
 const { description } = require('../../package')
 import { defaultTheme } from '@vuepress/theme-default'
+import { containerPlugin } from '@vuepress/plugin-container'
 
 module.exports = {
 
@@ -243,5 +244,8 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    containerPlugin({
+      type: 'info'
+    })
   ]
 }
