93e3e6887d0b1f0a67100c8ec516a781796969b7
DevJeff <45824907+DevChanQ@users.noreply.github.com>
2023-03-05 06:53:50 +0800

    commit

--- docs/src/.vuepress/client.js
+++ docs/src/.vuepress/client.js
@@ -1,7 +1,7 @@
 import { getDirname, path } from '@vuepress/utils'
 import { defineClientConfig } from '@vuepress/client'
 
-import CookbookLayout from './layouts/Layout.vue';
+import Layout from './layouts/Layout.vue';
 
 const __dirname = getDirname(import.meta.url)
 
@@ -9,13 +9,13 @@ export default defineClientConfig({
   enhance({ app, router, siteData }) {
   },
   setup(){},
-  layouts: { Layout: CookbookLayout },
-  alias: {
-    '@theme/Navbar.vue': path.resolve(__dirname, './components/Navbar.vue'),
-    '@theme/Page.vue': path.resolve(__dirname, './components/Page.vue'),
-    '@theme/Sidebar.vue': path.resolve(__dirname, './components/Sidebar.vue'),
-    '@theme/Onboarding.vue': path.resolve(__dirname, './components/Onboarding.vue'),
-    '@theme/ToggleSidebarButton.vue': path.resolve(__dirname, './components/ToggleSidebarButton.vue'),
-  },
+  layouts: { Layout },
+  // alias: {
+  //   '@theme/Navbar.vue': path.resolve(__dirname, './components/Navbar.vue'),
+  //   '@theme/Page.vue': path.resolve(__dirname, './components/Page.vue'),
+  //   '@theme/Sidebar.vue': path.resolve(__dirname, './components/Sidebar.vue'),
+  //   '@theme/Onboarding.vue': path.resolve(__dirname, './components/Onboarding.vue'),
+  //   '@theme/ToggleSidebarButton.vue': path.resolve(__dirname, './components/ToggleSidebarButton.vue'),
+  // },
   rootComponents: [],
 })
--- docs/src/.vuepress/components/Navbar.vue
+++ docs/src/.vuepress/components/Navbar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ToggleColorModeButton from '@theme/ToggleColorModeButton.vue'
-import ToggleSidebarButton from '@theme/ToggleSidebarButton.vue'
+import ToggleSidebarButton from './ToggleSidebarButton.vue'
 import { ref } from 'vue'
 import { useThemeLocaleData } from '@vuepress/theme-default/lib/client/composables/index.js'
 defineEmits(['toggle-sidebar'])
@@ -68,7 +68,7 @@ const navbar = ref(null)
     display: flex;
 
     align-items: center;
-    justify-content: end;
+    justify-content: flex-end;
   }
 
   &.fixed {
