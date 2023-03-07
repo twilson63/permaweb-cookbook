ce1d39893122da197c3b257211b6831d84ff26d1
DevJeff <45824907+DevChanQ@users.noreply.github.com>
2023-03-08 02:58:50 +0800

    add google calendar link

--- docs/src/.vuepress/components/Topbar.vue
+++ docs/src/.vuepress/components/Topbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="topbar">
-    <p>12pm ET / 5pm GMT every week on Wednesdays <a target="_blank" class="action" href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=M2Y4NXRtaWh1ZWd1a2ZzOTRha200NzlzNnUgY19hN2VjN2VmOTAzYTZlNGFjZjAwNWU5YjZlZTVkNGQxYzUyOGFiZmFjODRmYzM3NGY1YjIzZmQ0MzMzZTE4MzE0QGc&tmsrc=c_a7ec7ef903a6e4acf005e9b6ee5d4d1c528abfac84fc374f5b23fd4333e18314%40group.calendar.google.com">Add to Google Calendar</a></p>
+    <p>12pm ET / 5pm GMT every week on Wednesdays <a target="_blank" class="action" href="https://calendar.google.com/calendar/u/0?cid=Y182Nzk0ZDU3ZWRjNTM1NGIwN2IzZjllZWQ0MmY3NGZkMzc5M2EwNmU0OGExOWZjMmYzNzQ3MzViZjdlNWRlZGFiQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20">Add to Google Calendar</a></p>
   </div>
 </template>
 
