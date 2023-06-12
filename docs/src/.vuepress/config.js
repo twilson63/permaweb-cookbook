6a527167a40fa97327aa609c1e91127954d0be38
ropats16 <rohitcpathare@gmail.com>
2023-06-12 10:37:44 +0530

    Update config and add locales for English and Espanol

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,20 +1,28 @@
-import { description } from '../../package';
+import { description } from "../../package";
 
-import { defaultTheme } from '@vuepress/theme-default';
-import { containerPlugin } from '@vuepress/plugin-container';
-import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom';
-import { searchPlugin } from '@vuepress/plugin-search';
+import { defaultTheme } from "@vuepress/theme-default";
+import { containerPlugin } from "@vuepress/plugin-container";
+import { mediumZoomPlugin } from "@vuepress/plugin-medium-zoom";
+import { searchPlugin } from "@vuepress/plugin-search";
+
+import en from "./locales/en";
+import es from "./locales/es";
 
 export default {
-  base: '/',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Cooking with the Permaweb',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
-  description: description,
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    "/": {
+      lang: "en", // this will be set as the lang attribute on <html>
+      title: en.title,
+      description: description,
+    },
+    "/es/": {
+      lang: "es",
+      title: en.title,
+      description: description,
+    },
+  },
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
@@ -22,395 +30,63 @@ export default {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     [
-      'meta',
+      "meta",
       {
-        name: 'apple-mobile-web-app-status-bar-style',
-        content: 'black'
-      }
+        name: "apple-mobile-web-app-status-bar-style",
+        content: "black",
+      },
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
-        type: 'image/ico',
-        sizes: '16x16',
-        href: '/Permaweb_Cookbook.ico'
-      }
+        rel: "icon",
+        type: "image/ico",
+        sizes: "16x16",
+        href: "/Permaweb_Cookbook.ico",
+      },
     ],
-    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
-    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    ["link", { rel: "preconnect", href: "https://fonts.gstatic.com" }],
     [
-      'link',
+      "link",
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&display=swap'
-      }
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&display=swap",
+      },
     ],
     [
-      'script',
+      "script",
       {
         defer: true,
-        src: 'https://ackee-l09o.onrender.com/tracker.js',
-        'data-ackee-server': 'https://ackee-l09o.onrender.com',
+        src: "https://ackee-l09o.onrender.com/tracker.js",
+        "data-ackee-server": "https://ackee-l09o.onrender.com",
         async: true,
-        'data-ackee-domain-id': 'a6bf4de4-a529-452d-a611-6296c8af1b58'
-      }
-    ]
+        "data-ackee-domain-id": "a6bf4de4-a529-452d-a611-6296c8af1b58",
+      },
+    ],
   ],
 
   markdown: {
     code: {
-      lineNumbers: false
-    }
+      lineNumbers: false,
+    },
   },
 
   theme: defaultTheme({
     repo: "https://github.com/twilson63/permaweb-cookbook",
     editLink: true,
-    editLinkPattern: ':repo/edit/:branch/docs/src/:path',
-    colorMode: 'dark',
-    sidebar: [
-      {
-        text: 'Getting Started',
-        link: '/getting-started/index.html',
-        collapsible: true,
-        children: [
-          {
-            text: 'Welcome',
-            link: '/getting-started/welcome'
-          },
-          {
-            text: 'Hello World (CLI)',
-            link: '/getting-started/quick-starts/hw-cli'
-          },
-          {
-            text: 'Hello World (With Code)',
-            link: '/getting-started/quick-starts/hw-code'
-          },
-          {
-            text: 'Hello World (NodeJS)',
-            link: '/getting-started/quick-starts/hw-nodejs'
-          },
-          {
-            text: 'Contributing',
-            link: '/getting-started/contributing'
-          }
-        ]
-      },
-      {
-        text: 'Core Concepts',
-        link: '/concepts/index.html',
-        collapsible: true,
-        children: [
-          {
-            text: `Posting Transactions`,
-            link: `/concepts/post-transactions`,
-            collapsible: true,
-            children: [
-              {
-                text: 'arweave-js',
-                link: '/guides/posting-transactions/arweave-js'
-              },
-              {
-                text: 'bundlr.network',
-                link: '/guides/posting-transactions/bundlr'
-              },
-              {
-                text: 'dispatch',
-                link: '/guides/posting-transactions/dispatch'
-              }
-            ]
-          },
-          {
-            text: 'Metadata (Tags)',
-            link: '/concepts/tags'
-          },
-          {
-            text: `Querying`,
-            link: `/concepts/queryTransactions`
-          },
-          {
-            text: `Fetching Data`,
-            link: `/guides/http-api.md`
-          },
-          {
-            text: 'Transaction Types',
-            children: [
-              {
-                text: 'Bundles',
-                link: '/concepts/bundles'
-              },
-              {
-                text: 'Path Manifests',
-                link: '/concepts/manifests'
-              }
-            ]
-          },
-          {
-            text: 'Wallets and Keys',
-            link: '/concepts/keyfiles-and-wallets'
-          },
-          {
-            text: 'Permaweb',
-            link: '/concepts/permaweb',
-            collapsible: false,
-            children: [
-              {
-                text: 'Permaweb Applications',
-                link: '/concepts/permawebApplications'
-              },
-              {
-                text: 'Gateway Services',
-                link: '/concepts/gateways'
-              },
-              {
-                text: 'Bundling Services',
-                link: '/concepts/bundlers'
-              }
-            ]
-          },
-          {
-            text: 'SmartWeave',
-            link: '/concepts/smartweave',
-            collapsible: false,
-            children: [
-              {
-                text: 'Arweave Name System (ArNS)',
-                link: '/concepts/arns'
-              },
-              {
-                text: 'Atomic Tokens',
-                link: '/concepts/atomic-tokens.md'
-              },
-              {
-                text: 'Profit Sharing Tokens (PSTs)',
-                link: '/concepts/psts'
-              },
-              {
-                text: 'Vouch',
-                link: '/concepts/vouch'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        text: 'Guides',
-        link: '/guides/index.html',
-        collapsible: true,
-        children: [
-          {
-            text: 'ArProfile',
-            link: '/guides/arprofile'
-          },
-          {
-            text: 'DNS Integration',
-            collapsible: true,
-            children: [
-              {
-                text: 'Server Side',
-                link: '/guides/dns-integration/server-side'
-              },
-              {
-                text: 'Spheron',
-                link: '/guides/dns-integration/spheron'
-              }
-            ]
-          },
-          {
-            text: `Deploying Apps`,
-            collapsible: true,
-            children: [
-              {
-                text: 'arkb',
-                link: '/guides/deployment/arkb'
-              },
-              {
-                text: 'Bundlr',
-                link: '/guides/deployment/bundlr-cli'
-              },
-              {
-                text: 'Github Action',
-                link: '/guides/deployment/github-action'
-              }
-            ]
-          },
-          {
-            text: `Deploying PathManifests`,
-            link: '/guides/deploying-manifests/deployingManifests',
-            collapsible: false,
-            children: [
-              {
-                text: 'arweave.app',
-                link: '/guides/deploying-manifests/arweave-app'
-              },
-              {
-                text: 'ardrive',
-                link: '/guides/deploying-manifests/ardrive'
-              },
-              {
-                text: 'bundlr.network',
-                link: '/guides/deploying-manifests/bundlr'
-              }
-            ]
-          },
-          {
-            text: 'Deploying PSTs',
-            collapsible: false,
-            link: '/guides/deploying-psts'
-          },
-          {
-            text: 'Execution Machine',
-            collapsible: true,
-            children: [
-              {
-                text: 'Introduction',
-                link: '/guides/exm/intro.md'
-              },
-              {
-                text: 'API Token',
-                link: '/guides/exm/api.md'
-              },
-              {
-                text: 'JS SDK',
-                collapsible: true,
-                children: [
-                  {
-                    text: 'Execution Machine SDK',
-                    link: '/guides/exm/js-sdk/sdk-intro.md'
-                  },
-                  {
-                    text: 'Deploy with SDK',
-                    link: '/guides/exm/js-sdk/sdk-deploy.md'
-                  },
-                  {
-                    text: 'Write with SDK',
-                    link: '/guides/exm/js-sdk/sdk-write.md'
-                  },
-                  {
-                    text: 'Read with SDK',
-                    link: '/guides/exm/js-sdk/sdk-read.md'
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            text: 'GraphQL',
-            link: '/guides/querying-arweave/queryingArweave',
-            collapsible: false,
-            children: [
-              {
-                text: 'ArDB',
-                link: '/guides/querying-arweave/ardb'
-              },
-              {
-                text: 'ar-gql',
-                link: '/guides/querying-arweave/ar-gql'
-              },
-              {
-                text: 'Search Indexing Service',
-                link: '/guides/querying-arweave/search-indexing-service'
-              }
-            ]
-          },
-          {
-            text: 'SmartWeave',
-            collapsible: true,
-            children: [
-              {
-                text: 'Atomic Tokens',
-                link: '/guides/atomic-tokens/intro'
-              },
-              {
-                text: 'Vouch',
-                link: '/guides/vouch'
-              },
-              {
-                text: 'Warp',
-                collapsible: false,
-                children: [
-                  {
-                    text: 'Intro',
-                    link: '/guides/smartweave/warp/intro.md'
-                  },
-                  {
-                    text: 'Deploying Contracts',
-                    link: '/guides/smartweave/warp/deploying-contracts.md'
-                  },
-                  {
-                    text: 'Read Contract State',
-                    link: '/guides/smartweave/warp/readstate.md'
-                  },
-                  {
-                    text: 'Write Contract Interactions',
-                    link: '/guides/smartweave/warp/write-interactions.md'
-                  },
-                  {
-                    text: 'Evolve Contract',
-                    link: '/guides/smartweave/warp/evolve.md'
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            text: `Testing`,
-            collapsible: true,
-            children: [
-              {
-                text: 'arlocal',
-                collapsible: false,
-                link: '/guides/testing/arlocal'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        text: 'References',
-        link: '/references/index.html',
-        collapsible: true,
-        children: [
-          {
-            text: 'Bundling',
-            collapsible: false,
-            link: '/references/bundling'
-          },
-          {
-            text: 'GraphQL',
-            collapsible: false,
-            link: '/references/gql'
-          },
-          {
-            text: 'HTTP API',
-            collapsible: false,
-            link: '/references/http-api'
-          }
-        ]
-      },
-      {
-        text: 'Starter Kits',
-        link: '/kits/',
-        collapsible: true,
-        children: [
-          {
-            text: 'React',
-            link: '/kits/react/'
-          },
-          {
-            text: 'Svelte',
-            link: '/kits/svelte/'
-          },
-          {
-            text: 'Vue',
-            link: '/kits/vue/'
-          }
-        ]
-      }
-    ]
+    editLinkPattern: ":repo/edit/:branch/docs/src/:path",
+    layouts: {
+      "/": "./layouts/en/DefaultLayout.vue",
+      "/es/": "./layouts/es/DefaultLayout.vue",
+    },
+    colorMode: "dark",
+    sidebar: {
+      "/": en.sidebar,
+      "/es/": es.sidebar,
+    },
   }),
 
   /**
@@ -418,15 +94,15 @@ export default {
    */
   plugins: [
     mediumZoomPlugin({
-      selector: ':not(.not-zoomable)'
+      selector: ":not(.not-zoomable)",
     }),
     containerPlugin({
-      type: 'info'
+      type: "info",
     }),
     searchPlugin({
       locales: {
-        '/': {
-          placeholder: 'Search',
+        "/": {
+          placeholder: "Search",
         },
       },
     }),
@@ -434,7 +110,7 @@ export default {
 
   async onInitialized(app) {
     app.pages
-      .filter(page => page.path.slice(-1) === '/' && page.path !== '/')
-      .forEach(page => page.path += 'index.html');
+      .filter((page) => page.path.slice(-1) === "/" && page.path !== "/")
+      .forEach((page) => (page.path += "index.html"));
   },
 };
---
+++ docs/src/.vuepress/locales/es.js
@@ -0,0 +1,357 @@
+export default {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "Cocinando con la Permaweb",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  // description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html#sidebar
+   */
+
+  sidebar: [
+    {
+      text: "Comenzando",
+      link: "/es/getting-started/",
+      collapsible: true,
+      children: [
+        {
+          text: "Bienvenido",
+          link: "/es/getting-started/welcome",
+        },
+        {
+          text: "Hola Mundo (CLI)",
+          link: "/es/getting-started/quick-starts/hw-cli",
+        },
+        {
+          text: "Hola Mundo (Con Código)",
+          link: "/es/getting-started/quick-starts/hw-code",
+        },
+        {
+          text: "Hola Mundo (NodeJS)",
+          link: "/es/getting-started/quick-starts/hw-nodejs",
+        },
+        {
+          text: "Contribuyendo",
+          link: "/es/getting-started/contributing",
+        },
+      ],
+    },
+    {
+      text: "Conceptos Fundamentales",
+      link: "/es/concepts/",
+      collapsible: true,
+      children: [
+        {
+          text: "Publicación de Transacciones",
+          link: "/es/concepts/post-transactions",
+          collapsible: true,
+          children: [
+            {
+              text: "arweave-js",
+              link: "/es/guides/posting-transactions/arweave-js",
+            },
+            {
+              text: "bundlr.network",
+              link: "/es/guides/posting-transactions/bundlr",
+            },
+            {
+              text: "dispatch",
+              link: "/es/guides/posting-transactions/dispatch",
+            },
+          ],
+        },
+        {
+          text: `Metadatos (Etiquetas "Tags")`,
+          link: "/es/concepts/tags",
+        },
+        {
+          text: `Consultas`,
+          link: `/es/concepts/queryTransactions`,
+        },
+        {
+          text: `Obteniendo Datos`,
+          link: `/es/guides/http-api.md`,
+        },
+        {
+          text: "Tipos de Transacción",
+          children: [
+            {
+              text: "Paquetes",
+              link: "/es/concepts/bundles",
+            },
+            {
+              text: "Manifiestos de Ruta",
+              link: "/es/concepts/manifests",
+            },
+          ],
+        },
+        {
+          text: "Billeteras y Claves",
+          link: "/es/concepts/keyfiles-and-wallets",
+        },
+        {
+          text: "Permaweb",
+          link: "/es/concepts/permaweb",
+          collapsible: false,
+          children: [
+            {
+              text: "Aplicaciones Permaweb",
+              link: "/es/concepts/permawebApplications",
+            },
+            {
+              text: "Servicios de Puerta de Enlace",
+              link: "/es/concepts/gateways",
+            },
+            {
+              text: "Servicios de Agrupación",
+              link: "/es/concepts/bundlers",
+            },
+          ],
+        },
+        {
+          text: "SmartWeave",
+          link: "/es/concepts/smartweave",
+          collapsible: false,
+          children: [
+            {
+              text: "Sistema de Nombres Arweave (ArNS)",
+              link: "/es/concepts/arns",
+            },
+            {
+              text: "Tokens Atómicos",
+              link: "/es/concepts/atomic-tokens.md",
+            },
+            {
+              text: "Tokens de Participación en Ganancias (PSTs)",
+              link: "/es/concepts/psts",
+            },
+            {
+              text: "Vouch",
+              link: "/es/concepts/vouch",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Guías",
+      link: "/es/guides/",
+      collapsible: true,
+      children: [
+        {
+          text: "ArProfile",
+          link: "/es/guides/arprofile",
+        },
+        {
+          text: "Integración DNS",
+          collapsible: true,
+          children: [
+            {
+              text: "Lado del Servidor",
+              link: "/es/guides/dns-integration/server-side",
+            },
+            {
+              text: "Spheron",
+              link: "/es/guides/dns-integration/spheron",
+            },
+          ],
+        },
+        {
+          text: `Desplegando Aplicaciones`,
+          collapsible: true,
+          children: [
+            {
+              text: "arkb",
+              link: "/es/guides/deployment/arkb",
+            },
+            {
+              text: "Bundlr",
+              link: "/es/guides/deployment/bundlr-cli",
+            },
+            {
+              text: "Acción de Github",
+              link: "/es/guides/deployment/github-action",
+            },
+          ],
+        },
+        {
+          text: `Desplegando Manifiestos de Ruta`,
+          link: "/es/guides/deploying-manifests/deployingManifests",
+          collapsible: false,
+          children: [
+            {
+              text: "arweave.app",
+              link: "/es/guides/deploying-manifests/arweave-app",
+            },
+            {
+              text: "ardrive",
+              link: "/es/guides/deploying-manifests/ardrive",
+            },
+            {
+              text: "bundlr.network",
+              link: "/es/guides/deploying-manifests/bundlr",
+            },
+          ],
+        },
+        {
+          text: "Desplegando PSTs",
+          collapsible: false,
+          link: "/es/guides/deploying-psts",
+        },
+        {
+          text: "Execution Machine",
+          collapsible: true,
+          children: [
+            {
+              text: "Introducción",
+              link: "/es/guides/exm/intro.md",
+            },
+            {
+              text: "Token de API",
+              link: "/es/guides/exm/api.md",
+            },
+            {
+              text: "SDK de JS",
+              collapsible: true,
+              children: [
+                {
+                  text: "SDK de Execution Machine",
+                  link: "/es/guides/exm/js-sdk/sdk-intro.md",
+                },
+                {
+                  text: "Desplegar con SDK",
+                  link: "/es/guides/exm/js-sdk/sdk-deploy.md",
+                },
+                {
+                  text: "Escribir con SDK",
+                  link: "/es/guides/exm/js-sdk/sdk-write.md",
+                },
+                {
+                  text: "Leer con SDK",
+                  link: "/es/guides/exm/js-sdk/sdk-read.md",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          text: "GraphQL",
+          link: "/es/guides/querying-arweave/queryingArweave",
+          collapsible: false,
+          children: [
+            {
+              text: "ArDB",
+              link: "/es/guides/querying-arweave/ardb",
+            },
+            {
+              text: "ar-gql",
+              link: "/es/guides/querying-arweave/ar-gql",
+            },
+            {
+              text: "Servicio de Indexación de Búsqueda",
+              link: "/es/guides/querying-arweave/search-indexing-service",
+            },
+          ],
+        },
+        {
+          text: "SmartWeave",
+          collapsible: true,
+          children: [
+            {
+              text: "Tokens Atómicos",
+              link: "/es/guides/atomic-tokens/intro",
+            },
+            {
+              text: "Vouch",
+              link: "/es/guides/vouch",
+            },
+            {
+              text: "Warp",
+              collapsible: false,
+              children: [
+                {
+                  text: "Introducción",
+                  link: "/es/guides/smartweave/warp/intro.md",
+                },
+                {
+                  text: "Desplegar Contratos",
+                  link: "/es/guides/smartweave/warp/deploying-contracts.md",
+                },
+                {
+                  text: "Leer Estado del Contrato",
+                  link: "/es/guides/smartweave/warp/readstate.md",
+                },
+                {
+                  text: "Escribir Interacciones del Contrato",
+                  link: "/es/guides/smartweave/warp/write-interactions.md",
+                },
+                {
+                  text: "Evolucionar Contrato",
+                  link: "/es/guides/smartweave/warp/evolve.md",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          text: `Pruebas`,
+          collapsible: true,
+          children: [
+            {
+              text: "arlocal",
+              collapsible: false,
+              link: "/es/guides/testing/arlocal",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Referencias",
+      link: "/es/references/",
+      collapsible: true,
+      children: [
+        {
+          text: "Empaquetado",
+          collapsible: false,
+          link: "/es/references/bundling",
+        },
+        {
+          text: "GraphQL",
+          collapsible: false,
+          link: "/es/references/gql",
+        },
+        {
+          text: "API HTTP",
+          collapsible: false,
+          link: "/es/references/http-api",
+        },
+      ],
+    },
+    {
+      text: "Kits de Inicio",
+      link: "/es/kits/",
+      collapsible: true,
+      children: [
+        {
+          text: "React",
+          link: "/es/kits/react/",
+        },
+        {
+          text: "Svelte",
+          link: "/es/kits/svelte/",
+        },
+        {
+          text: "Vue",
+          link: "/es/kits/vue/",
+        },
+      ],
+    },
+  ],
+};
