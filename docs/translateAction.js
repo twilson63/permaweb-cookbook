ae2a5111fdd06843be7f118a39fbca6dfce9b57c
ropats16 <rohitcpathare@gmail.com>
2023-06-16 11:19:32 +0530

    Update translation scripts

--- docs/translateAction.js
+++ docs/translateAction.js
@@ -1,10 +1,9 @@
-const fs = require("fs");
 const path = require("path");
 const { Octokit } = require("@octokit/core");
-const { promisify } = require("util");
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
+// Init Octokit (utility to interact with GitHub API)
 const octokit = new Octokit({ auth: process.env.SPECIAL_ACCESS_TOKEN });
 
 async function main() {
@@ -14,18 +13,19 @@ async function main() {
 
   if (pullRequest) {
     console.log(`Checking for modified docs files...`);
-    //   // Get the modified files from the push
+    // Get the modified files from the push
+    // Selects only from "concepts", "getting-started", "guides", "kits", "references" folders
+    // Or the main "index.md" file
     const modifiedFiles = await getModifiedFiles(pullRequest);
-    //   for (const file in modifiedFiles) {
-    //     console.log(`This is the file ${file.filename}`);
-    //   }
+
+    // Looping over modified files
     for (const file of modifiedFiles) {
       console.log(`Reading file content for ${file}...`);
       // Reading file content
       const fileContent = await octokit.request(
         "GET /repos/{owner}/{repo}/contents/{path}",
         {
-          owner: "ropats16",
+          owner: "twilson63",
           repo: "permaweb-cookbook",
           path: file,
           headers: {
@@ -34,6 +34,7 @@ async function main() {
         }
       );
 
+      // File content is decoded from `base64` to `string`
       const decodedFileContent = atob(fileContent.data.content);
 
       console.log(`Here's the response: ${decodedFileContent} for ${file}...`);
@@ -41,43 +42,33 @@ async function main() {
       // Define the prompt for translation to Spanish
       const prompt = `As a linguistics professor who is an expert in English and Spanish, translate the following markdown text to Spanish while maintaining and translating the context in which the terms, phrases and sections have been created in the original text and keep in mind that the reader is familiar with some initial information about Arweave and blockchain infrastructure:\n\n${decodedFileContent}`;
 
-      console.log(`Writing translated file...`);
       // Translate the content to Spanish using the OpenAI API client
       const translatedContent = await translateTextToSpanish(prompt);
 
+      // Getting relative path of file in `src` folder
       const relativePath = file.split("docs/src");
 
-      console.log(
-        `This is the relative path: ${
-          relativePath[1]
-        } and its type is ${typeof relativePath}`
-      );
-
-      // Write the translated file to the "es" subfolder
+      // Creating new file path for translated file using relative path
       const translatedFilePath = path.join("docs/src/es", relativePath[1]);
 
-      console.log(
-        `This is the translated file path: ${translatedFilePath} and its type is ${typeof translatedFilePath}`
-      );
-
+      // Adding frontmatter to translation
       const markdownTranslatedContent =
         addMarkdownFormatting(translatedContent);
 
       console.log(`Writing translated file to ${translatedFilePath}...`);
 
-      // Check if path exists
+      // Check if file path for translated file already exists
       const dirStatus = await ensureDirectoryExists(translatedFilePath);
 
-      // Write content to file
+      // If file path does not exist, create new file
+      // Else, overwrite existing file (linked using the SHA)
       if (dirStatus.exists === false) {
         console.log("Creating new file...");
         await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-          owner: "ropats16",
+          owner: "twilson63",
           repo: "permaweb-cookbook",
           path: translatedFilePath,
-          message: `Translate ${translatedFilePath.substring(
-            translatedFilePath.lastIndexOf("/") + 1
-          )} to Spanish`,
+          message: `Translate ${translatedFilePath} to Spanish`,
           committer: {
             name: "ropats16",
             email: "rohitcpathare@gmail.com",
@@ -90,12 +81,10 @@ async function main() {
       } else if (dirStatus.exists === true) {
         console.log("Overwriting existing file...");
         await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-          owner: "ropats16",
+          owner: "twilson63",
           repo: "permaweb-cookbook",
           path: translatedFilePath,
-          message: `Translate ${translatedFilePath.substring(
-            translatedFilePath.lastIndexOf("/") + 1
-          )} to Spanish`,
+          message: `Translate ${translatedFilePath} to Spanish`,
           committer: {
             name: "ropats16",
             email: "rohitcpathare@gmail.com",
@@ -114,8 +103,9 @@ async function main() {
   }
 }
 
+// Get latest pull request
 async function getLatestPullRequest() {
-  const owner = "ropats16";
+  const owner = "twilson63";
   const repo = "permaweb-cookbook";
 
   // Get the latest push to the main branch
@@ -131,17 +121,15 @@ async function getLatestPullRequest() {
   const pullRequests = response.data;
 
   if (pullRequests.length > 0 && pullRequests[0].merged_at) {
-    console.log(
-      `**********************This is latest PR number ${pullRequests[0].number} ***************`
-    );
     return pullRequests[0];
   }
 
   return null;
 }
 
+// Get modified files from selected folders
 async function getModifiedFiles(pullRequest) {
-  const owner = "ropats16";
+  const owner = "twilson63";
   const repo = "permaweb-cookbook";
 
   const subfoldersToRead = [
@@ -174,15 +162,13 @@ async function getModifiedFiles(pullRequest) {
       )
     ) {
       filePaths = [...filePaths, file.filename];
-      console.log(
-        `//////////////The file at this path was changed: ${file.filename}/////////////`
-      );
     }
   }
 
   return filePaths;
 }
 
+// Function to translate text using OpenAI
 async function translateTextToSpanish(text) {
   // Create config for OpenAi and create instance
   const configuration = new Configuration({
@@ -198,12 +184,10 @@ async function translateTextToSpanish(text) {
     messages: [{ role: "user", content: text }],
     max_tokens: 4096,
   });
-
-  // return response.choices[0].text.trim();
-  console.log("This is res", response.data.choices[0].message.content);
   return response.data.choices[0].message.content;
 }
 
+// Function to check if translated file path already exists
 async function ensureDirectoryExists(filePath) {
   console.log(`Checking if ${filePath} exists...`);
   // Reading file content
@@ -211,7 +195,7 @@ async function ensureDirectoryExists(filePath) {
     const fileContent = await octokit.request(
       "GET /repos/{owner}/{repo}/contents/{path}",
       {
-        owner: "ropats16",
+        owner: "twilson63",
         repo: "permaweb-cookbook",
         path: filePath,
         headers: {
@@ -226,6 +210,7 @@ async function ensureDirectoryExists(filePath) {
   }
 }
 
+// Adds locale frontmatter to text
 function addMarkdownFormatting(text) {
   const frontmatter = `---
 locale: es
