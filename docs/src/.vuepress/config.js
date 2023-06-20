cca6f35a41fa5fe9bd540285d85d1a1088a98b40
DevJeff <45824907+DevChanQ@users.noreply.github.com>
2023-06-21 02:24:48 +0700

    slash redirect fix

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -107,4 +107,10 @@ export default {
       },
     }),
   ],
+
+  async onInitialized(app) {
+    app.pages
+      .filter((page) => page.path.slice(-1) === "/" && page.path !== "/")
+      .forEach((page) => (page.path += "index.html"));
+  },
 };
